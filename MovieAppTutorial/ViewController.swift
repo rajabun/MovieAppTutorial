b8b5d7e5fd6975b61b98f34e2cb80735165a775a
Muhammad Rajab <ichijo_rajab@hotmail.com>
2021-03-25 01:29:15 +0700

    Initial Commit

---
+++ MovieAppTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAppTutorial
+//
+//  Created by Muhammad Rajab Priharsanto on 25/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
